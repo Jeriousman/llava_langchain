3405de207fbfbdd05cb49bcc15faed2d2d920e0e
Hojun Seo <hojun0705@gmail.com>
2024-08-05 22:52:05 +0900

    latest commit before quitting the job

--- Dockerfile
+++ Dockerfile
@@ -10,17 +10,23 @@ RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC \
     libgl1-mesa-glx libglib2.0-0 libsm6 \
     wget curl cmake build-essential pkg-config \
     libxext6 libxrender-dev git
-    
+
 RUN apt-get clean && rm -rf /tmp/* /var/tmp/* \
  && python3 -m pip install --upgrade pip \
  && python3 -m pip install --upgrade setuptools
+
+
 
 COPY . /workspace
 WORKDIR /workspace
 
 RUN curl -fsSL https://ollama.com/install.sh | sh
-# RUN ollama serve
-# RUN ollama run llava-llama3:8b
+RUN pip3 install --no-cache-dir -Iv tqdm
 RUN pip3 install -r /workspace/requirements_llm_langchain.txt
+
+
+ENV LANGCHAIN_API_KEY=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 \
+    OPENAI_API_KEY=sk-proj-H6lBFUuqY8c9BKH3jwp5T3BlbkFJdsOWOeX8VKt4wi2W4b1h
+
 
 
--- llava_langchain.py
+++ llava_langchain.py
@@ -90,33 +90,6 @@ def take_inputs_then_answer(img, prompt:str):
     return response
 
 
-# prompt = PromptTemplate.from_template("tell me a joke about {subject}")
-
-# template = """
-# 너는 요리사야. 내가 가진 재료들을 갖고 만들 수 있는 요리를 추천하고, 그 요리의 레시피를 제시해줘.
-# 내가 가진 재료는 아래와 같아.
-
-# <재료>
-# {재료}
-
-# """
-
-
-# #ChatGPT에게 역할을 부여합니다.(위에서 정의한 Template 사용)
-# system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-
-# #사용자가 입력할 매개변수 template을 선언합니다.
-# human_template = "{재료}"
-# human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-
-# #ChatPromptTemplate에 system message와 human message 템플릿을 삽입합니다.
-# chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-
-# #ChatGPT API에 ChatPromptTemplate을 입력할 때, human message의 매개변수인 '재료'를 할당하여 전달합니다.
-# #이와 같은 방식을 통해 ChatGPT는 ChatPromptTemplate의 구성요소인 system message와 human message를 전달받아, 대답 생성에 활용합니다.
-# answer = mllm(chat_prompt.format_prompt(재료="양파, 계란, 사과, 빵").to_messages())
-# print(answer.content)
-
 
 # prompt = '''
 # You are an expert that checks if photos are really natural with real-world notions. 
